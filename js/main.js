ccb50cdb988f5439da0bfe5afd9e3e9cbdf6e49b
Tsetan Dorje <tsetangrace@gmail.com>
2022-11-05 16:14:41 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 50) {
+        nav.classList.add("header-scrolled")
+    } else {
+        nav.classList.remove("header-scrolled")
+    }
+}
+
+// Nav Hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse")
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show")
+    })
+})
+
+// Swiper Slider
+let swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3500,
+    },
+});
