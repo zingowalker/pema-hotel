5e56d4cf991a30250036e495e282980d085ddf1f
Tsetan Dorje <tsetangrace@gmail.com>
2022-12-02 19:09:16 +0530

    Service, counter, gallery and footer

--- js/main.js
+++ js/main.js
@@ -29,3 +29,25 @@ let swiper = new Swiper(".mySwiper", {
         delay: 3500,
     },
 });
+
+// counter design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 0, 800, 1000);
+    counter("count2", 50, 500, 1000);
+    counter("count3", 0, 400, 3000);
+    counter("count4", 0, 2000, 3000);
+});
